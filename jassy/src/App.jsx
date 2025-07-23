2a1910531fc9714aa6cfb0cac51c11fa0c791219
2005Andrei <ascurtuny@gmail.com>
2025-07-23 22:49:06 +0300

    update for deployment

--- jassy/src/App.jsx
+++ jassy/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Preloader from './components/Preloader';
 import Particles from './components/Particles';
 import Scene from './components/Scene';
 import Footer from './components/Footer';
-import Network from './components/NetworkSimulator/Network';
 import { GlobalStyles } from './styles';
-import RSAVisualizer from './components/RSA/RSAVisualizer';
-import AIWeaponizationAwareness from './components/rest/AIWeaponizationAwareness';
-import PasswordHashingDemonstrator from './components/rest/PasswordHashingDemonstrator';
-import IoTVulnerabilityScanner from './components/rest/IoTVulnerabilityScanner';
+
+// Lazy-load heavy components
+const Network = React.lazy(() => import('./components/NetworkSimulator/Network'));
+const RSAVisualizer = React.lazy(() => import('./components/RSA/RSAVisualizer'));
+const AIWeaponizationAwareness = React.lazy(() => import('./components/rest/AIWeaponizationAwareness'));
+const PasswordHashingDemonstrator = React.lazy(() => import('./components/rest/PasswordHashingDemonstrator'));
+const IoTVulnerabilityScanner = React.lazy(() => import('./components/rest/IoTVulnerabilityScanner'));
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -28,14 +30,16 @@ function App() {
       {!isLoading && (
         <>
           <Particles />
-          <Routes>
-            <Route path="/" element={<><Scene /><Footer /></>} />
-            <Route path="/card/card5" element={<AIWeaponizationAwareness />} />
-            <Route path="/card/card4" element={<IoTVulnerabilityScanner />} />
-            <Route path="/card/card3" element={<Network />} />
-            <Route path="/card/card2" element={<RSAVisualizer />} />
-            <Route path="/card/card1" element={<PasswordHashingDemonstrator />} />
-          </Routes>
+          <Suspense fallback={<div>Loading component...</div>}>
+            <Routes>
+              <Route path="/" element={<><Scene /><Footer /></>} />
+              <Route path="/card/card5" element={<AIWeaponizationAwareness />} />
+              <Route path="/card/card4" element={<IoTVulnerabilityScanner />} />
+              <Route path="/card/card3" element={<Network />} />
+              <Route path="/card/card2" element={<RSAVisualizer />} />
+              <Route path="/card/card1" element={<PasswordHashingDemonstrator />} />
+            </Routes>
+          </Suspense>
         </>
       )}
     </Router>
