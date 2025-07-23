6c099117a6731174ddd75e883ed280165f7bf88e
2005Andrei <ascurtuny@gmail.com>
2025-07-23 22:52:30 +0300

    update vite

--- jassy/vite.config.js
+++ jassy/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'vite-plugin-bundle-analyzer';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), visualizer()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Suppress warnings for now
+  },
+});
