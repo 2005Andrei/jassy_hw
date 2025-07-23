fb775335eb53a27d981755dc85cad0173b88f121
2005Andrei <ascurtuny@gmail.com>
2025-07-24 02:22:09 +0300

    this better work

--- jassy/vite.config.js
+++ jassy/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jassy_hw/',
+  base: '/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Suppress chunk size warnings
